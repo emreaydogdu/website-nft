d850bc8a633e864e37ed6be547b5ef7a1af331e5
EmreAydogdu <emre.aydogdu@facilioo.de>
2022-03-16 13:08:38 +0100

    add website

--- mintTiers.js
+++
@@ -1,8 +0,0 @@
-import { mint } from './mint.js';
-
-document.querySelectorAll('[tier]').forEach((element) => {
-  element.setAttribute('href', '#');
-  element.onclick = () => {
-    mint(1, Number(element.getAttribute("tier")));
-  }
-})
